6dc5ced05b33f0af0bc0a7b2f0ed573c26e255b3
Jenelle Schilcher <jschilcher8@gmail.com>
2021-07-26 21:13:20 -0500

    Axios get request with David and Golden

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import axios from "axios";
 import Title from "./Title/title";
 import "../components/app.css";
 
@@ -6,11 +7,27 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            mediaData: {},
         };
     };
 
+    componentDidMount() {
+        this.fetchMediaData();
+    }
+
+    async fetchMediaData() {
+        let response = await axios.get(
+            "http://localhost:5000/api/collections"
+        );
+        this.setState({
+            mediaData: response.data
+        });
+    }
+
     render() {
+        if(this.state.mediaData[0] === undefined) {
+            return <div>Loading...</div>
+        }
         return (
             <div>
                 <Title/> 
