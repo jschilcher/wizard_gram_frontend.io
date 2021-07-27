c05e19ff326e8be2d3a4456231ccc379edf76de2
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-27 19:46:46 -0400

    added Profile component w/ Golden and Jenelle

---
+++ src/components/Profile/profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function profile() {
+  return (
+    <div>
+      <header className="profile-header"></header>
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default profile;
--- src/components/Title/title.jsx
+++ src/components/Title/title.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import "../Title/title.css"
+import React from "react";
+import "../Title/title.css";
 
 function Title(props) {
-    return (
-        <div className="row row-spacer">
-            <div className="col-align" style={{padding: 0}}>
-                <div className="title-nav">
-                    <h1>WizardGram</h1>
-                </div>
-            </div>
+  return (
+    <div className="row row-spacer barcolor">
+      <div className="col-align" style={{ padding: 0 }}>
+        <div className="title-nav">
+          <h1>WizardGram</h1>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Title;
