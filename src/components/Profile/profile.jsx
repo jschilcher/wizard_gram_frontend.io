ddbf06002a120e5b138f48c11d92f74055f0ef84
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-28 19:23:22 -0400

    fixed useEffect comment

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -5,7 +5,7 @@ import "../Profile/profile.css";
 function Profile(props) {
   const [user, setUser] = useState({});
 
-  useEffect(() => {
+  // useEffect(() => {
   //   const fetchUser = async () => {
   //     const response = await axios
   //       .get("http://localhost:5000/collections/user")
