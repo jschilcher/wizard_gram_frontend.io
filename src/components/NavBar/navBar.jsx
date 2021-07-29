b03a129e7c89abe71621db087c89b39cd1a8e81d
Jenelle Schilcher <jschilcher8@gmail.com>
2021-07-29 18:53:01 -0500

    Add route for login to profile with David and Golden

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,21 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function NavBar() {
+const NavBar = ({user}) => {
     return(
-        <nav>
+        <div>
+            {user && <h4>Welcome {user.username}</h4>}
             <ul>
-                <Link to="/profile">
-                    <li>Profile</li>
-                </Link>
-                <Link to="/login">
-                    <li>Login</li>
-                </Link>
-                <Link to="/register">
-                    <li>Register</li>
-                </Link>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+                {!user &&
+                <React.Fragment>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    </React.Fragment>}
+                {user &&
+                <React.Fragment>
+                    <li>
+                        <Link to="/logout">Logout</Link>
+                    </li>
+                    </React.Fragment>}
             </ul>
-        </nav>
+        </div>
+        // <nav>
+        //     <ul>
+        //         <Link to="/profile">
+        //             <li>Profile</li>
+        //         </Link>
+        //         <Link to="/login">
+        //             <li>Login</li>
+        //         </Link>
+        //         <Link to="/register">
+        //             <li>Register</li>
+        //         </Link>
+        //     </ul>
+        // </nav>
     )
 }
 
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,24 +2,52 @@ import React, { Component } from "react";
 import axios from "axios";
 import Title from "./Title/title";
 import "../components/app.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 import Login from "./Login/login";
 import Profile from "../components/Profile/profile";
 import NavBar from "./NavBar/navBar";
 import Register from "./Register/register";
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    };
+    componentDidMount() {
+        const jwt = localStorage.getItem("token");
+        try{
+            const user = jwtDecode(jwt);
+            this.setstate({
+                user
+            });
+        } catch {
+
+        }
+    }
+    render() {    
+        const user = this.state.user;
   return (
     <div className="App">
       <Title />
-      <NavBar />
+      <NavBar user={user} />
       <Switch>
+        {/* <Route path="/login" exact component={Login} /> */}
+        <Route path="/profile" render={props => {
+            if(!user){
+                return <Redirect to="/login" />;
+            } else {
+                return <Profile {...props} user={user} />
+            }
+        }}
+        />
         <Route path="/login" exact component={Login} />
-        <Route path="/profile" exact component={Profile} />
-        <Route path="/register" exact component={Register} />
+        <Route path="/register" component={Register} />
+        
       </Switch>
     </div>
   );
+    }
 }
 
 export default App;
